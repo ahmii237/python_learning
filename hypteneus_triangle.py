05f08fba4e84941a087dca85498541610983626f
Mudasar Ahmad <mudasarofficial237@gmail.com>
2026-09-07 06:32:50 +0500

    day 2

---
+++ hypteneus_triangle.py
@@ -0,0 +1,7 @@
+import math
+height=float(input("Enter the height of triangle: "))
+base = float (input("enter the base of triangle: "))
+
+hypoteneus= math.sqrt(pow(height,2)+pow(base,2))
+
+print(f"The hypteneus of the triangle is {hypoteneus}")
